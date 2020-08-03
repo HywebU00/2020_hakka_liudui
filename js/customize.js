e170ec183f74e69fe63661a718bb8cce07419381
mingshuya <mingshuya@gmail.com>
2020-08-03 11:16:26 +0800

    六堆數位生態博物館HTML

---
+++ js/customize.js
@@ -0,0 +1,218 @@
+// 自行加入的JS請寫在這裡
+$(function() {
+    // 首頁輪播
+    $('.mpSlider').slick({
+        mobileFirst: true,
+        dots: true,
+        arrow: true,
+        infinite: true,
+        speed: 500,
+        autoplay: true,
+        fade: true,
+        lazyLoaded: true,
+        lazyLoad: 'ondemand',
+        ease: 'ease'
+    });
+    // Slider3D輪播
+    $('.Slider3D').slick({
+        mobileFirst: true,
+        dots: true,
+        arrow: true,
+        infinite: true,
+        speed: 500,
+        autoplay: true,
+        fade: true,
+        lazyLoaded: true,
+        lazyLoad: 'ondemand',
+        ease: 'ease'
+    });
+    // 廣告輪播
+    $('.adSlider').slick({
+        mobileFirst: true,
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        arrow: true,
+        lazyLoaded: true,
+        lazyLoad: 'ondemand',
+        ease: 'ease',
+        responsive: [{
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 5,
+                slidesToScroll: 1,
+                arrows: true
+            }
+        },{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                arrows: true
+            }
+        },{
+            breakpoint: 575,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                arrows: true
+            }
+        }]
+    });
+    //燈箱slick+lightBox組合
+    $('.cp_slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 1500,
+        pauseOnHover: true,
+        pauseOnFocus: true,
+        focusOnSelect: true,
+        accessibility: true,
+        lazyLoad: 'ondemand',
+        ease: 'ease',
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true
+            }
+        }, {
+            breakpoint: 545,
+            settings: {
+                arrows: true,
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                arrows: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false
+            }
+        }]
+    });
+    $('.cp_slider').slickLightbox({
+        caption: 'caption',
+        lazyLoad: 'ondemand',
+        useHistoryApi: 'true',
+        ease: 'ease',
+        lazy: true
+    });
+     // cp_photo
+     $('.Slider-for').on('init reInit afterChange', function(event, slick, currentSlide) {
+        var i = (currentSlide ? currentSlide : 0) + 1;
+        $('.controls').html(i + '/' + slick.slideCount);
+    });
+     $('.Slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        swipe: false,
+        swipeToSlide: false,
+        lazyLoad: 'ondemand',
+        asNavFor: '.Slider-nav',
+        infinite: true
+    });
+     $('.Slider-nav').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        asNavFor: '.Slider-for',
+        dots: true,
+        arrows: true,
+        lazyLoad: 'ondemand',
+        focusOnSelect: true,
+        infinite: true
+    });
+
+
+ });
+
+//狀態篩選
+$(function(){
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight){
+          content.style.maxHeight = null;
+      } else {
+          content.style.maxHeight = content.scrollHeight + "px";
+      } 
+  });
+  }
+})
+
+//Timeline
+
+
+$('.slider-timeline').slick({
+    slidesToShow: 12,
+    slidesToScroll: 1,
+    dots: false,
+    centerMode: false,
+    focusOnSelect: true,
+    arrows: true, //左右箭頭
+    infinite: false, //無限輪播
+    draggable: true, //滑鼠可以拖曳
+    autoplay: false, //自動播放
+    responsive: [{
+        breakpoint: 1199,
+        settings: {
+            slidesToShow: 8,
+            slidesToScroll: 1,
+        }
+    }, {
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 6,
+            slidesToScroll: 1,
+        }
+    }, {
+        breakpoint: 600,
+        settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+        }
+    }, {
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+        }
+    }]
+});
+
+
+var slideLength = $('.slider-timeline div').length,
+slideIndex = $('.slide-time div').index();
+console.log(slideLength);
+console.log(slideIndex);
+$('.slider-timeline').children('div').eq(slideIndex).find('a').off().click(function(e) {
+    slideIndex = $(this).parent().index();
+    $('.lp').find('a:first').focus();
+    $(this).parent().addClass('now');
+    console.log(slideIndex);
+    e.preventDefault();
+
+});
+$('.lp').find('a:last').focusout(function(e) {
+    $('.slider-timeline').children('.now').next('div').find('a').focus();
+});
+
+
+
